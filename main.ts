9e69154ebc777c547358997129cb2b874cadec03
iSpeakNerd <96841384+iSpeakNerd@users.noreply.github.com>
2024-11-18 10:36:49 +0700

    integrate apis

---
+++ main.ts
@@ -0,0 +1,12 @@
+import getFids from './lib/get-fids.ts';
+import dotenv from 'dotenv';
+import getPoapWallets from './lib/poap-wallets.ts';
+dotenv.config({ path: '.env.local' });
+
+const POAP_EVENT_ID = process.env.POAP_EVENT_ID! || '180427';
+
+async function main() {
+  const wallets = await getPoapWallets(POAP_EVENT_ID);
+  const fids = await getFids(wallets);
+}
+main();

74d1561f97fb5bccb21a524b0ebc77818cced649
iSpeakNerd <96841384+iSpeakNerd@users.noreply.github.com>
2024-11-26 14:19:59 -0800

    Update cast.ts
    remove env vars

--- cast.ts
+++ cast.ts
@@ -1,17 +1,12 @@
 import { createCastUrl } from './lib/warpcast-urls.ts';
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
 
 /**
  * Replace these with your values for frame cast
  */
-const ANNOUNCEMENT_TEXT =
-  process.env.ANNOUNCEMENT_TEXT! ||
-  `Welcome new frens! If you played a game and got a /poap from me at /devcon love to hear from you in /tabletop! 
+const ANNOUNCEMENT_TEXT = `Welcome new frens! If you played a game and got a /poap from me at /devcon love to hear from you in /tabletop! 
   Click Start to get your channel invite!`;
-const FRAME_URL =
-  process.env.FRAME_URL! || 'https://poap-invites-frame.vercel.app/api';
-const CHANNEL_KEY = process.env.CHANNEL_KEY! || 'tabletop';
+const FRAME_URL = 'https://poap-invites-frame.vercel.app/api';
+const CHANNEL_KEY = 'tabletop';
 
 (() => {
   const castUrl = createCastUrl({
